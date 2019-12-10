3ba68679ef82a51eed254817ea9768b2183f9a95
tetheve <tetheve@btslivh.eu>
2019-12-10 18:00:04 +0100

    ajout cout

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g1
+ *
+ * Created on 10 décembre 2019, 17:55
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "test"<<endl;
+    return 0;
+}
+
